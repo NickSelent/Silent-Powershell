b63fa2e641e73d6b72389fc3709aa46d0005e6ae
Nick Selent <nickselent@yahoo.com>
2017-08-16 21:33:20 -0400

    New repository

---
+++ MyScript.ps1
@@ -0,0 +1,13 @@
+﻿Add-Type -AssemblyName System.speech
+$tts = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+$Phrase = '
+<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US">
+    <voice xml:lang="en-US" name="Microsoft Zira Desktop">
+    <prosody rate="1" pitch="high">
+        <p>Your barn door is open.</p>
+    </prosody>
+    </voice>
+</speak>
+'
+$tts.SpeakSsml($Phrase)
